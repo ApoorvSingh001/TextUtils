e315eb2e1010c58a4078600270be2d61305a10f7
Apoorv <apoorvsingh967@gmail.com>
2023-11-08 00:34:14 +0530

    vid-8 simple dark mode

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import TextForm from './components/TextForm';
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About';
 function App() {
   return (
     <>
     <Navbar title="TextUtils" about="AboutMe"/> 
-    <div className="container my-3">
-    <TextForm heading= "Enter text to Analyse"/>
+    <div className="container my-5">
+    {/* <TextForm heading= "Enter text to Analyse"/> */}
+    <About/>
     </div>
-   
+    
   </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,14 +7,21 @@ const handleUpClick=()=>{
      console.log(newText);
     setText(newText);
 }
+const handleLowClick=()=>{
+  let newText=text.toLowerCase();
+   console.log(newText);
+  setText(newText);
+}
 const handleOnChange=(event)=>{
     console.log("On Change");
     setText(event.target.value)
 }
 const handleClickText=()=>{
-    setText(" ");
+    if(text==='Enter text here')
+    setText("");
 }
   return (
+    <>
 <div>
 
   <h1 className="heading">{props.heading}</h1>
@@ -22,7 +29,16 @@ const handleClickText=()=>{
   <textarea className="form-control" value={text}  onChange={handleOnChange} onClick={handleClickText} id="exampleFormControlTextarea1" rows="9"></textarea>
   <br/>
   <button className="btn btn-primary"  onClick={handleUpClick}>ToUpperCase</button>
+  <button className="btn btn-primary mx-3"  onClick={handleLowClick}>ToLowerCase</button>
+  </div>
+  <div className="container my-3">
+    <h1>Your text summary</h1>
+    <p>{text.split(' ').length} words and {text.length} characters</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
   </div>
 </div>
+
+</>
   );
 }
